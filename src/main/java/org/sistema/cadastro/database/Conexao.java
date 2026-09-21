548d0612ca58d0bfcf3bb3125dfd82c32f511151
andynnnfw <and345melo@gmail.com>
2026-09-21 15:11:31 -0300

    feat: completed all the system

---
+++ src/main/java/org/sistema/cadastro/database/Conexao.java
@@ -0,0 +1,18 @@
+package org.sistema.cadastro.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class Conexao {
+
+    public static Connection getConnection() {
+        try {
+            String url = "jdbc:mysql://localhost:3306/cadastro";
+            String user = "root";
+            String password = "";
+
+            return DriverManager.getConnection(url, user, password);
+        }catch (Exception e){
+            throw new RuntimeException("erro na conexao" + e.getMessage());
+        }
+    }
+}
